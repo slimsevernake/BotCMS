60f099e17b9dbca8c09303d076bdd37de7eda92e
mvoevodskiy <mv@aaa0.ru>
2019-09-22 02:11:12 +0300

    Renamed class BotCMSTools to BcTools and moved to it's own file

--- botcms.js
+++ botcms.js
@@ -1,3 +1,4 @@
+const BcTools = require('./bctools');
 const drivers = {
     tg: require('./telegram'),
     vk: require('./vkontakte'),
@@ -16,7 +17,7 @@ class BotCMS {
         this.schema = {};
         this.scripts = {};
         this.commands = [];
-        this.T = new BotCMSTools();
+        this.T = new BcTools();
         this.defaultStep = {
             trigger: {
                 type: 'text',
@@ -398,119 +399,6 @@ class BotCMS {
     launch (middleware, ...middlewares) {
         this.standard('launch', middleware, ...middlewares);
     }
-}
-
-class BotCMSTools {
-    constructor (BC) {
-        this.BC = BC;
-    }
-
-    empty (variable) {
-        return variable === undefined
-            || variable === null
-            || variable === false
-            || variable === ''
-            || (Array.isArray(variable) && variable.length === 0);
-    }
-
-    emptyExtracted (path, target = window) {
-        const extracted = this.extract(path, target);
-        return this.empty(extracted);
-    }
-
-    isArray (data) {
-        console.log('IS ARRAY. TYPE: ' + Object.prototype.toString.call(data));
-        return Object.prototype.toString.call(data) === '[object Array]'
-    }
-
-    isString (data) {
-        return (typeof data === "string" || data instanceof String);
-    }
-
-    inArray (value, array) {
-        for(var i = 0; i < array.length; i++){
-            if(value === array[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    isCommand (value) {
-        return typeof value === "string" && value.startsWith('/');
-    }
-
-    isChildrenPath (path) {
-        return path.endsWith('.c') || path === 'c';
-    }
-
-    checkTrigger (message, triggerObject) {
-        let trigger = null;
-        let result = false;
-        if (!this.empty(triggerObject) && triggerObject.hasOwnProperty('type')) {
-            if (triggerObject.type === 'text' && message.type === 'text') {
-                // console.log('CHECK TRIGGER. TYPE TEXT');
-                trigger = triggerObject.value;
-                if (Array.isArray(trigger)) {
-                    result = trigger.indexOf(message.text) > -1;
-                    // result = this.inArray(message.text, trigger);
-                    // console.log('CHECK TRIGGER. IS ARRAY');
-                } else {
-                    result = trigger === message.text.toString();
-                    // console.log('CHECK TRIGGER. IS STRING');
-                }
-            }
-
-            if (triggerObject.type === 'regexp' && message.type === 'text') {
-                if (Array.isArray(triggerObject.value)) {
-                    for (const re of triggerObject.value) {
-                        trigger = new RegExp(re, 'gim');
-                        result = message.text.toString().search(trigger) !== -1;
-                        if (result) {
-                            break;
-                        }
-                    }
-                    console.log('CHECK TRIGGER. IS ARRAY');
-                } else {
-                    console.log('CHECK TRIGGER. IS STRING');
-                    trigger = new RegExp(triggerObject.value, 'gim');
-                    result = message.text.toString().search(trigger) !== -1;
-                }
-            }
-
-            if (triggerObject.type === 'method') {
-                let func = 'myObj.method.myFunc';
-                let path = func.split('.');
-                trigger = window;
-                for (let i in path) {
-                    trigger = trigger[path[i]];
-                }
-            }
-            // console.log('CHECK TRIGGER. message, result, trigger', message, result, triggerObject);
-        }
-        return result;
-    }
-
-    extract (path, target = window) {
-        path = path.split('.');
-        while (path.length > 0) {
-            let step = path.shift();
-            if (!this.empty(target) && target.hasOwnProperty(step)) {
-                target = target[step];
-            } else {
-                return undefined;
-            }
-        }
-        return target;
-    }
-
-    extractAnswerThread (path) {
-        const pathParts = path.split('.');
-        return !this.empty(pathParts[1]) ? pathParts[1] : undefined;
-    }
-
-
-
 }
 
 module.exports = BotCMS;
